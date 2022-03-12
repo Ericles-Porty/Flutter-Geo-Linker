c71f83d039d8e3e585da6ea1a5a334cee937d264
Ericles-Porty <ericlesdsantos@gmail.com>
2022-03-12 19:08:30 -0300

    Revert "Revert "Final""
    This reverts commit 518c0ccb3f2df67865eadf3e5dc62bff76420e19.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:geo_serialization/models/user.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+const google = "https://www.google.com/maps/place/";
+
+void launchURL(String _url) async {
+  if (!await launch(_url)) throw 'Could not launch $_url';
+}
 
 Future<List<User>> getHttp() async {
   var response = await Dio().get('https://jsonplaceholder.typicode.com/users');
@@ -9,6 +16,7 @@ Future<List<User>> getHttp() async {
   return users;
 }
 
+String getUrl(String lng, String lat) => (lat + ',' + lng);
 void main() {
   runApp(const MyApp());
 }
@@ -31,12 +39,13 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> url = <String>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,6 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
             return const Center(child: CircularProgressIndicator());
           }
           List<User> listaUsuarios = snapshot.data as List<User>;
+
           return Column(
             children: [
               Expanded(
@@ -54,13 +64,59 @@ class _MyHomePageState extends State<MyHomePage> {
                   itemCount: listaUsuarios.length,
                   scrollDirection: Axis.vertical,
                   itemBuilder: (context, index) {
-                    return Container(
-                      child: Text('latitude: ' + listaUsuarios[index].address.geo.lat + ' longitude: ' + listaUsuarios[index].address.geo.lng),
+                    url.add(google +
+                        getUrl(listaUsuarios[index].address.geo.lng,
+                            listaUsuarios[index].address.geo.lat));
+                    return Card(
+                      margin: const EdgeInsets.all(8),
+                      elevation: 10,
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 250,
+                            height: 100,
+                            color: Colors.black12,
+                            padding: const EdgeInsets.all(8),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Nome: ' + listaUsuarios[index].name),
+                                  Text('Usuario: ' +
+                                      listaUsuarios[index].username),
+                                  Text('Email: ' + listaUsuarios[index].email),
+                                  Text('Telefone: ' +
+                                      listaUsuarios[index].phone),
+                                ]),
+                          ),
+                          Container(color: Colors.black,
+                            padding: const EdgeInsets.all(2),
+                            height: 100,
+                            width: 100,
+                            child: Image.asset(
+                              'assets/images/$index.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(width: 5),
+                          Expanded(
+                            child: TextButton(
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Colors.black12)),
+                              onPressed: () {
+                                launchURL(url.elementAt(index));
+                              },
+                              child: const Text('Visitar'),
+                            ),
+                          ),
+                          const SizedBox(width: 5),
+                        ],
+                      ),
                     );
                   },
-                  separatorBuilder: (context, index) => const SizedBox(
-                    width: 20,
-                  ),
+                  separatorBuilder: (context, index) =>
+                      const SizedBox(width: 10),
                 ),
               ),
             ],
--- lib/models/address.dart
+++ lib/models/address.dart
@@ -9,7 +9,12 @@ class Address {
   late String zipcode;
   late Geo geo;
 
-  Address({required this.street, required this.suite, required this.city, required this.zipcode, required this.geo});
+  Address(
+      {required this.street,
+      required this.suite,
+      required this.city,
+      required this.zipcode,
+      required this.geo});
 
   Address.fromJson(Map<String, dynamic> json) {
     street = json['street'];
@@ -25,9 +30,7 @@ class Address {
     data['suite'] = suite;
     data['city'] = city;
     data['zipcode'] = zipcode;
-    if (geo != null) {
-      data['geo'] = geo!.toJson();
-    }
+    data['geo'] = geo.toJson();
     return data;
   }
 }
